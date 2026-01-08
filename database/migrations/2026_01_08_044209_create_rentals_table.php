2bdf642351465510d910ed5a63cf53fe87801769
almer2304 <muhammadalmer2304@gmail.com>
2026-01-08 12:08:05 +0700

    Making migration for tooktook

---
+++ database/migrations/2026_01_08_044209_create_rentals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('item_id')->constrained()->cascadeOnDelete();
+            $table->date('start_date');
+            $table->date('due_date');
+            $table->date('returned_at')->nullable();
+            $table->enum('status', [
+                'pending',
+                'approved',
+                'rejected',
+                'returned',
+                'late'
+            ])->default('pending');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rentals');
+    }
+};
